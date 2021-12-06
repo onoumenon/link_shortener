a9c34214480cd6a9450097c02951615415bb710f
onoumenon <tanhuitian@gmail.com>
2021-12-06 18:02:57 +0800

    build url if url given is not a full http or https uri

--- app/controllers/link_controller.rb
+++ app/controllers/link_controller.rb
@@ -6,7 +6,8 @@ class LinkController < ApplicationController
     if link.save
       render json: {
         base_url: request.base_url,
-        shortcode: shortcode
+        shortcode: shortcode,
+        short_url: "#{request.base_url}/#{shortcode}"
       }
     else
       render json: { message: 'URL is invalid', errors: link.errors }, status: :bad_request
@@ -17,7 +18,15 @@ class LinkController < ApplicationController
     link = Link.find_by(shortcode: params[:shortcode])
 
     if link
-      redirect_to link.url, status: :moved_permanently
+      uri = URI.parse(link.url)
+      if uri.instance_of?(URI::Generic)
+        host, path = uri.path.split '/', 2
+        uri = URI::HTTP.build host: host,
+                              path: "/#{path}",
+                              query: uri.query,
+                              fragment: uri.fragment
+      end
+      redirect_to uri.to_s, status: :moved_permanently
       nil
     else
       render json: { message: 'Link not found' }, status: :not_found
