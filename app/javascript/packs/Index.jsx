983084d9fd43dbda7dbec573a17b54a9a776eb2d
onoumenon <tanhuitian@gmail.com>
2021-12-06 18:02:30 +0800

    show form component for url in index page

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -1,20 +1,95 @@
-import React from "react"
+import React, { useState } from "react"
 import ReactDOM from "react-dom"
-import PropTypes from "prop-types"
 
-const Hello = (props) => <div>Hello {props.name}!</div>
+import Button from "react-bootstrap/Button"
+import Stack from "react-bootstrap/Stack"
+import Form from "react-bootstrap/Form"
+import Alert from "react-bootstrap/Alert"
+import Card from "react-bootstrap/Card"
+import axios from "axios"
+// Importing the Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css"
 
-Hello.defaultProps = {
-  name: "David",
-}
+const Index = () => {
+  const [url, setUrl] = useState("")
+  const [shortUrl, setShortUrl] = useState(null)
+  const [show, setShow] = useState(false)
+  const [isCopied, setIsCopied] = useState(false)
 
-Hello.propTypes = {
-  name: PropTypes.string,
+  async function copyTextToClipboard(text) {
+    if ("clipboard" in navigator) {
+      return await navigator.clipboard.writeText(text)
+    } else {
+      return document.execCommand("copy", true, text)
+    }
+  }
+
+  const handleCopyClick = async () => {
+    try {
+      const res = await copyTextToClipboard(shortUrl)
+      setIsCopied(true)
+      setTimeout(() => {
+        setIsCopied(false)
+      }, 1500)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const handleSubmit = async () => {
+    try {
+      const res = await axios.post("/link", {
+        url,
+      })
+      setShow(false)
+      setShortUrl(res.data.short_url)
+    } catch (err) {
+      setShow(true)
+    }
+  }
+
+  return (
+    <div className="col-md-5 mx-auto mt-5">
+      {show && (
+        <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+          <p>Unable to shorten URL as link is invalid.</p>
+        </Alert>
+      )}
+
+      <Stack direction="horizontal" gap={3}>
+        <Form.Control
+          onChange={(e) => {
+            setUrl(e.target.value)
+          }}
+          value={url}
+          className="me-auto"
+          placeholder="Enter a URL to shorten"
+          type="text"
+        />
+        <Button variant="primary" onClick={handleSubmit}>
+          Submit
+        </Button>
+      </Stack>
+
+      {shortUrl && (
+        <Card className="text-center mt-3">
+          <Card.Body>
+            <Card.Text>
+              <a href={shortUrl}>{shortUrl}</a>
+            </Card.Text>
+            <Button variant="primary" onClick={handleCopyClick}>
+              {isCopied ? "Copied!" : "Copy"}
+            </Button>
+          </Card.Body>
+        </Card>
+      )}
+    </div>
+  )
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <Index name="React" />,
     document.body.appendChild(document.createElement("div")),
   )
 })
